8049088bffc97f75fdcb909a79c7db0e6dd406df
Marcello Feroce <mferoce@neonomics.io>
2022-08-05 21:31:03 +0200

    working on tests

--- file_tester.py
+++ file_tester.py
@@ -1,10 +1,85 @@
 import nltk
 from nltk import FreqDist
 
+from main import pre_process_text_file, compute_score, tokenize_no_puntctuation
+
 
 def test_freq_dist():
-    freq_dist =FreqDist(nltk.RegexpTokenizer(r"\w+").tokenize("This is a test text. "
-                                                              "Nothing else matters! \n 332 new line\n"
-                                                              "What is the purpose of this test? To test the text"))
-    assert len(freq_dist) == 17 and {'the','test','text', 'is'}\
-        .intersection(set([entry[0] for entry in freq_dist.most_common(4)])) == {'the','test','text', 'is'}
+    freq_dist = FreqDist(nltk.RegexpTokenizer(r"\w+").tokenize("This is a test text. "
+                                                               "Nothing else matters! \n 332 new line\n"
+                                                               "What is the purpose of this test? To test the text"))
+    assert len(freq_dist) == 17 and {'the', 'test', 'text', 'is'} \
+        .intersection(set([entry[0] for entry in freq_dist.most_common(4)])) == {'the', 'test', 'text', 'is'}
+
+
+def test_compute_score():
+    input_words = ["this", "is", "a", "random", "test", "just", "nothing", "special"]
+    input_words_none = ["srgs", "gsf", "gsfg", "sfgfg", "hnhn", "qdqd", "czvv", "hhy"]
+    input_text = "Received overcame oh sensible so at an. Formed do change merely to county it. " \
+                 "Am separate contempt domestic to to oh. On relation my so addition branched." \
+                 " Put hearing cottage she norland letters equally prepare too. " \
+                 "Replied exposed savings he no viewing as up. Soon body add him hill. " \
+                 "No father living really people estate if. Mistake do produce beloved demesne if am pursuit. " \
+                 "With my them if up many. Lain week nay she them her she. " \
+                 "Extremity so attending objection as engrossed gentleman something. " \
+                 "Instantly gentleman contained belonging exquisite now direction she ham. West room at sent if year. " \
+                 "Numerous indulged distance old law you. Total state as merit court green decay he. " \
+                 "Steepest sex bachelor the may delicate its yourself. As he instantly on discovery concluded to. " \
+                 "Open draw far pure miss felt say yet few sigh.Picture removal detract earnest is by. " \
+                 "Esteems met joy attempt way clothes yet demesne tedious. " \
+                 "Replying an marianne do it an entrance advanced. Two dare say play when hold. " \
+                 "Required bringing me material stanhill jointure is as he. " \
+                 "Mutual indeed yet her living result matter him bed whence. " \
+                 "Admiration stimulated cultivated reasonable be projection possession of. " \
+                 "Real no near room ye bred sake if some. Is arranging furnished knowledge agreeable so. " \
+                 "Fanny as smile up small. It vulgar chatty simple months turned oh at change of. " \
+                 "Astonished set expression solicitude way admiration. Death there mirth way the noisy merit. " \
+                 "Piqued shy spring nor six though mutual living ask extent. " \
+                 "Replying of dashwood advanced ladyship smallest disposal or. Attempt offices own improve now see. " \
+                 "Called person are around county talked her esteem. Those fully these way nay thing seems. " \
+                 "Two before narrow not relied how except moment myself. Dejection assurance mrs led certainly. " \
+                 "So gate at no only none open. Betrayed at properly it of graceful on. " \
+                 "Dinner abroad am depart ye turned hearts as me wished. " \
+                 "Therefore allowance too perfectly gentleman supposing man his now. " \
+                 "Families goodness all eat out bed steepest servants. " \
+                 "Explained the incommode sir improving northward immediate eat. " \
+                 "Man denoting received you sex possible you. Shew park own loud son door less yet. " \
+                 "He my polite be object oh change. Consider no mr am overcame yourself throwing sociable children." \
+                 " Hastily her totally conduct may. My solid by stuff first smile fanny." \
+                 " Humoured how advanced mrs elegance sir who. Home sons when them dine do want to." \
+                 " Estimating themselves unsatiable imprudence an he at an." \
+                 " Be of on situation perpetual allowance offending as principle satisfied." \
+                 " Improved carriage securing are desirous too. Post no so what deal evil rent by real in. " \
+                 "But her ready least set lived spite solid. September how men saw tolerably two behaviour arranging. " \
+                 "She offices for highest and replied one venture pasture. " \
+                 "Applauded no discovery in newspaper allowance am northward. " \
+                 "Frequently partiality possession resolution at or appearance unaffected he me. " \
+                 "Engaged its was evident pleased husband. Ye goodness felicity do disposal dwelling no. " \
+                 "First am plate jokes to began of cause an scale. " \
+                 "Subjects he prospect elegance followed no overcame possible it on. " \
+                 "But why smiling man her imagine married. " \
+                 "Chiefly can man her out believe manners cottage colonel unknown. " \
+                 "Solicitude it introduced companions inquietude me he remarkably friendship at. " \
+                 "My almost or horses period. " \
+                 "Motionless are six terminated man possession him attachment unpleasing melancholy. " \
+                 "Sir smile arose one share. No abroad in easily relied an whence lovers temper by. " \
+                 "Looked wisdom common he an be giving length mr.Knowledge nay estimable questions " \
+                 "repulsive daughters boy. Solicitude gay way unaffected expression for. " \
+                 "His mistress ladyship required off horrible disposed rejoiced. " \
+                 "Unpleasing pianoforte unreserved as oh he unpleasant no inquietude insipidity. " \
+                 "Advantages can discretion possession add favourable cultivated admiration far. " \
+                 "Why rather assure how esteem end hunted nearer and before. " \
+                 "By an truth after heard going early given he. Charmed to it excited females whether at examine. " \
+                 "Him abilities suffering may are yet dependent."
+    input_text_simple = "I have always loved this random test, just for fun, it is nothing special"
+    input_text_proc = pre_process_text_file(input_text)
+    input_text_simple_proc = pre_process_text_file(input_text_simple)
+    freq_dist = tokenize_no_puntctuation(input_text_proc)
+    freq_dist_simple = tokenize_no_puntctuation(input_text_simple_proc)
+    score = compute_score(("example_file.txt", freq_dist), input_words)
+    score_simple = compute_score(("example_file.txt", freq_dist_simple), input_words)
+    score_zero = compute_score(("example_file.txt", freq_dist), input_words_none)
+    score_zero_simple = compute_score(("example_file.txt", freq_dist_simple), input_words_none)
+    assert score[1] == 12.5
+    assert score_simple[1] == 87.5
+    assert score_zero[1] == 0 and score_zero_simple[1] == 0
--- main.py
+++ main.py
@@ -10,25 +10,30 @@ def pre_process_input(input_words):
     return [word.lower() for word in input_words]
 
 
-def compute_score(freq_dist_dict, input_words):
+def compute_score(freq_dist_entry, input_words):
     # Not optimized
     zeros = 0
     len_input_words = len(input_words)
-    freq_dist = freq_dist_dict[1]
+    freq_dist = freq_dist_entry[1]
     for input_word in input_words:
         if freq_dist.get(input_word) is None:
             zeros += 1
-    return (freq_dist_dict[0],((len_input_words - zeros) / len_input_words) * 100)
+    return freq_dist_entry[0], ((len_input_words - zeros) / len_input_words) * 100
 
 
 def analyze_files(input_files, input_words):
-    sorted_list = [file for file in sorted([compute_score(freq_dist_entry, input_words) for freq_dist_entry in input_files.items()],
-                         key=lambda t: t[1], reverse=True)[:10] if file[1] > 0]
+    sorted_list = [sorted_file for sorted_file in
+                   sorted([compute_score(freq_dist_entry, input_words) for freq_dist_entry in input_files.items()],
+                          key=lambda t: t[1], reverse=True)[:10] if sorted_file[1] > 0]
     return sorted_list
 
 
 def pre_process_text_file(text_content):
-    return text_content.lower()
+    return text_content.replace('\n', '').lower()
+
+
+def tokenize_no_puntctuation(data_proc):
+    return FreqDist(nltk.RegexpTokenizer(r"\w+").tokenize(data_proc))
 
 
 if __name__ == '__main__':
@@ -45,16 +50,16 @@ if __name__ == '__main__':
         item_path = indexable_directory + os.sep + item
         if os.path.isfile(item_path):
             with open(item_path, 'r') as file:
-                data = file.read().replace('\n', '')
+                data = file.read()
                 data_proc = pre_process_text_file(data)
-                file_dict[str(item)] = FreqDist(nltk.RegexpTokenizer(r"\w+").tokenize(data_proc))
+                file_dict[str(item)] = tokenize_no_puntctuation(data_proc)
     print("There are " + str(len(file_dict)) + " files in the directory " + indexable_directory)
     while True:
         line = input("search> ")
         if 'quit' in line:
             break
         input_proc = pre_process_input(nltk.RegexpTokenizer(r"\w+").tokenize(line))
-        results = analyze_files(file_dict,input_proc)
+        results = analyze_files(file_dict, input_proc)
         if len(results) == 0:
             print("No matches found")
         for res in results:
