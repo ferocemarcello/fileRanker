b6ec7fc7cdcf619b79e2bc791b907d6fe1d24820
Marcello Feroce <mferoce@neonomics.io>
2022-08-06 09:55:22 +0300

    refactoring

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import os
 import sys
 
 import nltk
-from nltk import FreqDist
+from nltk import FreqDist, PorterStemmer
 
+ps = PorterStemmer()
 
-def pre_process_input(input_words, stemming=False, stopwords=False):
+def pre_process_input(input_words, lower=True, stemming=False, stopwords=False):
     # remove stopwords and punctuation and lower
-    return set([word.lower() for word in tokenize_no_puntctuation(input_words)])
+    return set(tokenize_no_puntctuation(input_words, lower=lower, stemming=stemming, stopwords=stopwords))
 
 
 def compute_score(freq_dist, input_words):
@@ -42,34 +43,40 @@ def analyze_files(input_files, input_words):
     return sorted_list
 
 
-def pre_process_text_file(text_content, stemming=False, stopwords=False):
-    return FreqDist(tokenize_no_puntctuation(text_content.replace('\n', '').lower()))
+def pre_process_text_file(text_content, lower=True, stemming=False, stopwords=False):
+    return FreqDist(tokenize_no_puntctuation(text_content.replace('\n', ''), lower=lower, stemming=stemming,
+                    stopwords=stopwords))
 
 
-def tokenize_no_puntctuation(data_proc):
+def tokenize_no_puntctuation(data_proc, lower=True, stemming=False, stopwords=False):
+    if stemming:
+        return [ps.stem(w,to_lowercase=lower) for w in nltk.RegexpTokenizer(r"\w+").tokenize(data_proc)]
+    if lower:
+        return [w.lower() for w in nltk.RegexpTokenizer(r"\w+").tokenize(data_proc)]
     return nltk.RegexpTokenizer(r"\w+").tokenize(data_proc)
 
 
-def fetch_files(indexable_directory, stemming=False, stopwords=False):
+def fetch_files(indexable_directory, lower=True, stemming=False, stopwords=False):
     items = os.listdir(indexable_directory)
     file_dict = dict()
     for item_name in items:
         item_path = indexable_directory + os.sep + item_name
         if os.path.isfile(item_path):
             with open(item_path, 'r') as file:
-                file_dict[str(item_name)] = pre_process_text_file(file.read(), stemming=stemming, stopwords=stopwords)
+                file_dict[str(item_name)] = pre_process_text_file(file.read(),
+                                                                  lower=lower, stemming=stemming, stopwords=stopwords)
     print("There are " + str(len(file_dict)) + " files in the directory " + indexable_directory)
     return file_dict
 
 
-def get_user_input_loop(file_dict, stemming=False, stopwords=False):
+def get_user_input_loop(file_dict, lower=True, stemming=False, stopwords=False):
     while True:
         line = input("search> ")
         if 'quit' == line:
             print("The program is over")
             return 0
         try:
-            input_proc = pre_process_input(line, stemming, stopwords)
+            input_proc = pre_process_input(line, lower=lower, stemming=stemming, stopwords=stopwords)
             print("User input processed to " + str(input_proc))
             results = analyze_files(file_dict, input_proc)
         except:
@@ -97,11 +104,11 @@ def main(args):
              "(press Y/y for yes, or any other key for no)").lower() == "y":
         stopwords = True
     indexable_directory = args[0]
-    file_dict = fetch_files(indexable_directory, stemming=stemming, stopwords=stopwords)
+    file_dict = fetch_files(indexable_directory, lower=True,stemming=stemming, stopwords=stopwords)
     if len(file_dict) == 0:
         print("The program quits since there are no files to be analyzed")
         return 1
-    return get_user_input_loop(file_dict, stemming=stemming, stopwords=stopwords)
+    return get_user_input_loop(file_dict, lower=True, stemming=stemming, stopwords=stopwords)
 
 
 if __name__ == '__main__':
