2208aea96096b68055f027e452243c0d4748791c
Marcello Feroce <mferoce@neonomics.io>
2022-08-06 09:34:12 +0300

    refactoring

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import nltk
 from nltk import FreqDist
 
 
-def pre_process_input(input_words):
+def pre_process_input(input_words, stemming=False, stopwords=False):
     # remove stopwords and punctuation and lower
     return set([word.lower() for word in tokenize_no_puntctuation(input_words)])
 
@@ -42,34 +42,34 @@ def analyze_files(input_files, input_words):
     return sorted_list
 
 
-def pre_process_text_file(text_content):
-    return tokenize_no_puntctuation(text_content.replace('\n', '').lower())
+def pre_process_text_file(text_content, stemming=False, stopwords=False):
+    return FreqDist(tokenize_no_puntctuation(text_content.replace('\n', '').lower()))
 
 
 def tokenize_no_puntctuation(data_proc):
-    return FreqDist(nltk.RegexpTokenizer(r"\w+").tokenize(data_proc))
+    return nltk.RegexpTokenizer(r"\w+").tokenize(data_proc)
 
 
-def fetch_files(indexable_directory):
+def fetch_files(indexable_directory, stemming=False, stopwords=False):
     items = os.listdir(indexable_directory)
     file_dict = dict()
     for item_name in items:
         item_path = indexable_directory + os.sep + item_name
         if os.path.isfile(item_path):
             with open(item_path, 'r') as file:
-                file_dict[str(item_name)] = pre_process_text_file(file.read())
+                file_dict[str(item_name)] = pre_process_text_file(file.read(), stemming=stemming, stopwords=stopwords)
     print("There are " + str(len(file_dict)) + " files in the directory " + indexable_directory)
     return file_dict
 
 
-def get_user_input_loop(file_dict):
+def get_user_input_loop(file_dict, stemming=False, stopwords=False):
     while True:
         line = input("search> ")
         if 'quit' == line:
             print("The program is over")
             return 0
         try:
-            input_proc = pre_process_input(line)
+            input_proc = pre_process_input(line, stemming, stopwords)
             print("User input processed to " + str(input_proc))
             results = analyze_files(file_dict, input_proc)
         except:
@@ -86,13 +86,22 @@ def main(args):
     # pipreqs --force
     print("Welcome")
     if len(args) == 0:
-        raise Exception('No directory given to index')
+        print('No directory given to index')
+        return 1
+    stemming = False
+    stopwords = False
+    if input("Do you want to stem the input files and the word input?(press Y/y for yes, or any other key for no)") \
+            .lower() == "y":
+        stemming = True
+    if input("Do you want to remove stopwords from the input files and the word input?"
+             "(press Y/y for yes, or any other key for no)").lower() == "y":
+        stopwords = True
     indexable_directory = args[0]
-    file_dict = fetch_files(indexable_directory)
+    file_dict = fetch_files(indexable_directory, stemming=stemming, stopwords=stopwords)
     if len(file_dict) == 0:
         print("The program quits since there are no files to be analyzed")
         return 1
-    return get_user_input_loop(file_dict)
+    return get_user_input_loop(file_dict, stemming=stemming, stopwords=stopwords)
 
 
 if __name__ == '__main__':
